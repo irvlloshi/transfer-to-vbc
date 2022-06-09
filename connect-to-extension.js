6d72ece636cf42d257c82e8b8e9ee408822d1828
irv.lloshi@vonage.com <irv.lloshi@vonage.com>
2022-06-09 12:28:14 -0400

    first commit

---
+++ connect-to-extension.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const onInboundCall = (request, response) => {
+    const ncco = [{
+    action: 'connect',
+      from: 18576540469, // Replace with CLI you'd want to display on VBC
+  
+      endpoint: [{
+        type: 'vbc',
+        extension: 914889 // Replace with extension in VBC
+        
+      }]
+    }]
+  
+    response.json(ncco)
+    console.log(ncco);
+  }
+  
+  app.get('/webhooks/answer', onInboundCall)
+  app.post('/webhooks/events', (req, res) => {
+    console.log(req.body);
+  });
+  
+  app.listen(3000)
+  
